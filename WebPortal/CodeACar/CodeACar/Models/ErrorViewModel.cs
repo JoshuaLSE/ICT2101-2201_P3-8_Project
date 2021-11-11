09d2b66ae82f6d377e4a3e0102e2fa672eabdac7
Matt1998 <tsekinping2010@gmail.com>
2021-11-11 16:42:04 +0800

    Created Web Portal Framework structure (To be commited directly to main)

---
+++ WebPortal/CodeACar/CodeACar/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CodeACar.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
