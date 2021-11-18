786db2fc464de6a0a3eada29c35ac133f065a42b
alamsah2 <alamsah2@live.com>
2021-11-18 18:52:19 +0800

    init

---
+++ WebPortal/CodeACar/Models/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CodeACar.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { }
+    }
+}
