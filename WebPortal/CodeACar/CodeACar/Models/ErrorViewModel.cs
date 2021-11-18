1e78c481caed3413b08c0f678aa6a8674670ba0c
alamsah2 <alamsah2@live.com>
2021-11-18 15:32:12 +0800

    remove old project
    having conflict issues

--- WebPortal/CodeACar/CodeACar/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CodeACar.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
