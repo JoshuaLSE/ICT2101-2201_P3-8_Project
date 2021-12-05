dc71557ecc57910361b751235202054a718f5a9c
Matt1998 <tsekinping2010@gmail.com>
2021-12-05 19:41:43 +0800

    Working on Challenge History #61

---
+++ WebPortal/CodeACar/Models/ChallengeHistory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace CodeACar.Models
+{
+    public class ChallengeHistory
+    {
+        public int ChallengeHistoryId { get; set; }
+        public bool IsCompleted { get; set; }
+        public string Command { get; set; }
+        public int ChallengeId { get; set; }
+        public string Statistics { get; set; }
+        [DisplayName("StudentId")]
+        public int UserId { get; set; }
+
+        public Challenge Challenge { get; set; }
+        public User User { get; set; }
+    }
+}
