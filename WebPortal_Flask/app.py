e0b4eafa88a18282839877415c8a03572ea0e529
Joshua Leong <90120658+JoshuaLSE@users.noreply.github.com>
2021-12-06 19:00:31 +0800

    Update app.py
    Bugfix - POST json to main webportal

--- WebPortal_Flask/app.py
+++ WebPortal_Flask/app.py
@@ -1,8 +1,8 @@
 import json
-import secrets
 import sqlite3
 
-from flask import Flask, jsonify, request, session
+import requests as requests
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -32,7 +32,9 @@ def generate_database():
         '''INSERT INTO car(entry,details)VALUES ('command','MOV[1:1:3] MOV[4:1:3] MOV[3:1:3] MOV[1:1:3]')''')
     con.commit()
     print("Commands inserted")
-    cur.execute('''INSERT INTO car(entry,details)VALUES ('stat','noneyet')''')
+    cur.execute('''INSERT INTO car(entry,details)VALUES ('stat','none')''')
+    con.commit()
+    cur.execute('''INSERT INTO car(entry,details)VALUES ('challenge','none')''')
     con.commit()
     cur.execute('''SELECT * FROM car''')
     results = cur.fetchall()
@@ -61,26 +63,50 @@ def carAcknowledgement(data):  # put application's code here
     con.commit()
     cur.execute('''UPDATE car SET details = "''' + data + '''" WHERE entry = "stat"''')
     con.commit()
-    return "OK"
+    status = ""
+    identify = ""
+    cur.execute('''SELECT details FROM car WHERE entry = "stat"''')
+    for row in cur.fetchone():
+        status = row
+    x = []
+    for status in status.split("."):
+        if status != '':
+            r_loc = status.find("R")
+            l_loc = status.find("L")
+            e_loc = status.find("E")
+            y = {"Right": str(status[r_loc + 1:l_loc]),
+                 "Left": str(status[l_loc + 1:e_loc]),
+                 "Emergency": str(status[e_loc + 1:])}
+            x.append(y)
+    cur.execute('''SELECT details FROM car WHERE entry = "challenge"''')
+    for row in cur.fetchone():
+        identify = row
+    final = {"statistic": x, "challengeHistoryId": identify}
+    response = requests.post('http://localhost:46290/Student/UpdateCarStatistic', json=final)
+    return final
 
 
 # This is the GET method
-@app.route('/setCommand<string:command>')
-def setCarInstructions(command):
+@app.route('/setCommand<string:command>/<string:identifier>')
+def setCarInstructions(command, identifier):
     con = sqlite3.connect('car_details')
     cur = con.cursor()
     cur.execute('''UPDATE car SET details = "''' + command + '''" WHERE entry = "command"''')
+    con.commit()
+    cur.execute('''UPDATE car SET details = "''' + identifier + '''" WHERE entry = "challenge"''')
     con.commit()
     return "Updated: " + command
 
 
 # This is the POST method
-@app.route('/setCarCommand', methods=['POST'])
+@app.route('/sendCarCommand', methods=['POST'])
 def recvFromAsProject():
     content = request.json
     con = sqlite3.connect('car_details')
     cur = con.cursor()
     cur.execute('''UPDATE car SET details = "''' + str(content['carCommand']) + '''" WHERE entry = "command"''')
+    cur.execute(
+        '''UPDATE car SET details = "''' + str(content['challengeHistoryId']) + '''" WHERE entry = "challenge"''')
     con.commit()
     return content
 
@@ -90,20 +116,25 @@ def fetchCarStatistics():
     con = sqlite3.connect('car_details')
     cur = con.cursor()
     status = ""
+    identify = ""
     cur.execute('''SELECT details FROM car WHERE entry = "stat"''')
     for row in cur.fetchone():
         status = row
-    x = json.loads('[]')
+    x = []
     for status in status.split("."):
         if status != '':
             r_loc = status.find("R")
             l_loc = status.find("L")
             e_loc = status.find("E")
-            y = {"Right speed": str(status[r_loc + 1:l_loc]),
-                 "Left speed": str(status[l_loc + 1:e_loc]),
-                 "Emergency Break": str(status[e_loc + 1:])}
+            y = {"Right": str(status[r_loc + 1:l_loc]),
+                 "Left": str(status[l_loc + 1:e_loc]),
+                 "Emergency": str(status[e_loc + 1:])}
             x.append(y)
-    return jsonify(x)
+    cur.execute('''SELECT details FROM car WHERE entry = "challenge"''')
+    for row in cur.fetchone():
+        identify = row
+    final = {"statistic": x, "challengeHistoryId": identify}
+    return jsonify(final)
 
 
 if __name__ == '__main__':
